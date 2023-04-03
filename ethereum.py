a87704ee21488abc017db784d741fbade998b89c
Monkey Balls <37158637+unclevito2017@users.noreply.github.com>
2023-04-02 23:56:55 -0400

    Add files via upload

---
+++ ethereum.py
@@ -0,0 +1,35 @@
+import requests
+import time
+import winsound
+
+addresses_file = "eaddresses.txt"
+api_key = "WAZDPMPBA23WZ2JADYSDKGNYJK66CG6WCC"
+api_url = "https://api.etherscan.io/api"
+
+def check_balance(address):
+    params = {
+        "module": "account",
+        "action": "balance",
+        "address": address,
+        "tag": "latest",
+        "apikey": api_key
+    }
+    response = requests.get(api_url, params=params)
+    data = response.json()
+    balance = float(data["result"]) / (10 ** 18)
+    print(f"Balance for address {address}: {balance} ETH")
+    if balance > 0:
+        winsound.Beep(1000, 1000)
+    if balance > 0:
+        with open("found.txt", "a") as f:
+            f.write(f"{address}: {balance} ETH\n")
+
+with open(addresses_file) as f:
+    addresses = f.read().splitlines()
+
+ # write the balance to the found.txt file if it's positive
+    
+
+for address in addresses:
+    check_balance(address)
+    time.sleep(1)
